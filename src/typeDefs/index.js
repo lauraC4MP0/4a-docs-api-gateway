fb014f8e54486b9cb122ec387b06c2865176cddd
Anama1127 <92000105+Anama1127@users.noreply.github.com>
2021-12-05 18:29:12 -0500

    Update index.js
    Se agrego la extensión de patientTypeDefs

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -3,9 +3,10 @@
 
 const reportEvolutionTypeDefs = require('./reportEvolution_type_defs');
 const authTypeDefs= require('./authTypeDefs');
+const patientTypeDefs= require('./patientTypeDefs');
 
 
 //Se unen
-const schemasArrays = [reportEvolutionTypeDefs, authTypeDefs];
+const schemasArrays = [reportEvolutionTypeDefs, authTypeDefs, patientTypeDefs];
 //Se exportan
 module.exports = schemasArrays;
