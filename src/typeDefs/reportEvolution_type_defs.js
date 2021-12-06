bc82189f056c7739583b051cb6559f73d626d5d0
JulyBarragan <julyazmin.barragan@gmail.com>
2021-12-06 15:24:35 -0500

    se crea el Resolver de reportEvolution

--- src/typeDefs/reportEvolution_type_defs.js
+++ src/typeDefs/reportEvolution_type_defs.js
@@ -1,7 +1,7 @@
 const { gql } = require('apollo-server');
 const ReportEvolutionTypeDefs = gql `
 type  ReportEvolution {
-date: LocalDate!
+date: String!
 idPatient: Long!
 age: Integer!
 weight: String!
@@ -18,10 +18,20 @@ medicines: String!
 }
 input ReportEvolutionInput {
 idPatient: Long!
+age: Integer!
+weight: String!
+reason_for_consultation: String!
+current_illness: String!
+background: String!
+diseases: String!
+allergies: String!
+hereditary_family_history: String!
+physical_exam: String!
+medicines: String!
 
 }
 extend type Query {
-reportEvolutionByUsername(id: Long!): [ReportEvolution]
+reportEvolutionById(id: Long!): [ReportEvolution]
 }
 extend type Mutation {
 createReportEvolution(reportEvolution: ReportEvolutionInput!): ReportEvolution
