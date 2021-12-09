3bed64fb4fc306c43babc7d58a52f8ec233201a2
fcandanoza <fcandanoza@gmail.com>
2021-12-09 16:21:38 -0500

    guardando cambios reportEvolution_resolver

--- src/resolvers/reportEvolution_resolver.js
+++ src/resolvers/reportEvolution_resolver.js
@@ -1,12 +1,7 @@
 const reportEvolutionResolver ={
     Query:{
-<<<<<<< HEAD
-        reportEvolutionById: async({_, idPatient} , {dataSources, id}) => {
-            idCon =(await dataSources.patient_api.getId(id)).idPatient
-=======
         reportEvolutionById: async({_, idPatient} , {dataSources, id})=>{
             idCon =(await dataSources.patient_api.getId(id)).id
->>>>>>> fc3816ee122b226f5b336ba724673290f35bb478
             if(idPatient == idCon)
                 return await dataSources.reportEvolution_api.reportByidPatient(idPatient)
             else
@@ -18,15 +13,6 @@ const reportEvolutionResolver ={
 
 
     Mutation: {
-<<<<<<< HEAD
-        createReportEvolution: async({_, idPatient} , {dataSources, id}) => {
-            idCon =(await dataSources.patient_api.getId(id)).idPatient
-            if(reportEvolution.idPatient =idCon)
-                return await dataSources.reportEvolution_api.createReport(reportEvolution);
-            else
-                return null
-        }
-=======
      createReportEvolution: async({_, reportEvolutionInput} , {dataSources, id})=>{
         idCon =(await dataSources.patient_api.getId(id)).id
         if(reportEvolutionInput.idPatient == idCon)
@@ -34,7 +20,6 @@ const reportEvolutionResolver ={
          else
          return null
     }
->>>>>>> fc3816ee122b226f5b336ba724673290f35bb478
     }
 };
 module.exports=reportEvolutionResolver;
