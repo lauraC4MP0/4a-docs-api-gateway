8be4c09a412b84b95d11b061ef2bab45fd5374d8
fcandanoza <fcandanoza@gmail.com>
2021-12-11 11:34:25 -0500

    Corrección query loginId por archivo authentication

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -6,23 +6,23 @@ const authentication = async ({ req }) => {
     if (token == '')
         return { userIdToken: null }
     else {
-        try {
+        //try {
             let requestOptions = {
                 method: 'POST', headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ token }), redirect: 'follow'
             };
             let response = await fetch(
-                `${serverConfig.auth_api_url}/verifyToken/`,
+                `${serverConfig.auth_api_url}verifyToken/`,
                 requestOptions)
             if (response.status != 200) {
                 console.log(response)
                 throw new ApolloError(`SESION INACTIVA - ${401}` + response.status, 401)
             }
             return { userIdToken: (await response.json()).UserId };
-        }
-        catch (error) {
+        //}
+        /*catch (error) {
             throw new ApolloError(`TOKEN ERROR: ${500}: ${error}`, 500);
-        }
+        }*/
     }
 }
 module.exports = authentication;
